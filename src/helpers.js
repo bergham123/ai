0cd3691c2de98ed680c75876f0eb54b3c21d5905
bergham123 <bergham1998@gmail.com>
2026-07-18 20:54:25 +0000

    Add CORS headers function to helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -75,3 +75,13 @@ export function checkDailyLimit(userData, limit) {
     }
     return { allowed: true };
 }
+
+
+export function corsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Max-Age": "86400",
+  };
+}
